635b495ca70e31c7e948cf455c2fd8c25db9bd2a
Utku Yıldız <utkuyildiz7033@gmail.com>
2022-08-17 00:40:34 +0300

    2.C

--- src/day1Git.java
+++ src/day1Git.java
@@ -1,6 +1,7 @@
 public class day1Git {
     public static void main(String[] args) {
         System.out.println("Hello git 1. deneme");
+        System.out.println("Hello git 2. deneme");
 
     }
 }
